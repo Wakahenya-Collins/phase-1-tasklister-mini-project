04f6420de7b098b8292bd8f680aeada8c62aba02
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-06-22 23:47:26 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const task = input.value; 
+
+    
+    const listItem = document.createElement('li');
+    listItem.textContent = task;
+
+    taskList.appendChild(listItem); 
+
+    input.value = ''; 
+
+  });
 });
